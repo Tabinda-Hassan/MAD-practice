d56b7a339b2fe32700c00ac65ff63f263ebac69e
Tabinda-Hassan <tabindahassan279@gmail.com>
2025-02-23 01:04:55 +0500

    JavaScript Code

---
+++ Week#2_Array_of_Objects.js
@@ -0,0 +1,19 @@
+let objArr = [
+    {
+        name: 'john',
+        age: 12,
+        gender: 'male'
+    },
+    {
+        name: 'jane',
+        age: 20,
+        gender: 'female'
+    },
+    {
+        name: 'Julie',
+        age: 22,
+        gender: 'female'
+    }
+];
+let names = objArr.map(obj => obj.name);
+console.log(objArr.map);
